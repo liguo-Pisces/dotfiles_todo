bb49663c517bbbcd52a5554e86e8445f2bda6756
Li Guo <guolee226@gmail.com>
2021-01-06 15:21:09 +0800

    update

--- roles/emacs/files/lisp/m-note.el
+++ roles/emacs/files/lisp/m-note.el
@@ -1,2 +1,29 @@
+(require 'ox-publish)
+(setq org-publish-project-alist
+      '(
+        ("blog-notes"
+         :base-directory "~/.blog/notes/"
+         :base-extension "org"
+         :publishing-directory "~/.blog/html/"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+         :auto-preamble t
+         :section-numbers nil
+         :author "LiGuo"
+         :emal "guolee226@gmail.com"
+         :auto-sitemap t
+         :sitemap-filename "sitemap.org"
+         :sitemap-title "Sitemap"
+         :sitemap-sort-files anti-chronologically
+         :sitemap-file-entry-format "%d %t")
+        ("blog-static"
+         :base-directory "~/.blog/notes/"
+         :base-extension "css|js|png|jpg|gif|pdf|mp3|ogg|swf"
+         :publishing-directory "~/.blog/html/"
+         :recursive t
+         :publishing-function org-publish-attachment)
+        ("blog" :components ("blog-notes" "blog-static"))
+        ))
 
 (provide 'm-note)
