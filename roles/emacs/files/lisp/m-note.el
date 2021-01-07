20b62e9bbb2a0791adcb3e9ea629cb6cbbbf8a82
Li Guo <guolee226@gmail.com>
2021-01-07 17:49:56 +0800

    update

--- roles/emacs/files/lisp/m-note.el
+++ roles/emacs/files/lisp/m-note.el
@@ -1,29 +1,26 @@
+(set org-html-head-include-default-style nil
+     org-html-head "")
+
 (require 'ox-publish)
 (setq org-publish-project-alist
       '(
-        ("blog-notes"
-         :base-directory "~/.blog/notes/"
+        ("blog-pages"
+         :base-directory "~/.blog/org/"
          :base-extension "org"
+         :html-extension "html"
          :publishing-directory "~/.blog/html/"
-         :recursive t
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4
-         :auto-preamble t
+         :publishing-function (org-html-publish-to-html)
+         :auto-sitemap nil
          :section-numbers nil
-         :author "LiGuo"
-         :emal "guolee226@gmail.com"
-         :auto-sitemap t
-         :sitemap-filename "sitemap.org"
-         :sitemap-title "Sitemap"
-         :sitemap-sort-files anti-chronologically
-         :sitemap-file-entry-format "%d %t")
+         :table-of-contents t
+         :recursive t)
         ("blog-static"
-         :base-directory "~/.blog/notes/"
+         :base-directory "~/.blog/org/"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
          :publishing-directory "~/.blog/html/"
-         :recursive t
-         :publishing-function org-publish-attachment)
-        ("blog" :components ("blog-notes" "blog-static"))
+         :publishing-function org-publish-attachment
+         :recursive t)
+        ("blog" :components ("blog-pages" "blog-static"))
         ))
 
 (provide 'm-note)
